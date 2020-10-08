3bf011a7c3201ddae57cf271b38a2bc8852a9dd0
VojtechFrommelTUL <72199527+VojtechFrommelTUL@users.noreply.github.com>
2020-10-08 17:48:34 +0200

    Uloha 9,10 - DU1
    problém s desetinným dělením -> automatické zaokrouhlování (kdo to kdy viděl -_-).

--- alg1_du1_frommel/src/alg1_du1_frommel/Alg1_du1_frommel.java
+++ alg1_du1_frommel/src/alg1_du1_frommel/Alg1_du1_frommel.java
@@ -20,21 +20,53 @@ public class Alg1_du1_frommel {
     
     private static void Uloha9a10()
     {
+        //deklarace a inicializace konstant
         final int POKUTA_ZA_KMH = 1000;
         
+        //deklarace proměnných
+        double maxPovolenaRychlost;
+        double delkaUseku;
+        int hodPocatek;
+        int minPocatek;
+        int secPocatek;
+        int hodKonec;
+        int minKonec;
+        int secKonec;
+        double rozdilCasu;
+        double rozdilRychlosti;
+        double prumRychlost;
+        double vysePokuty;
+        double prumRychlostKMH;
+        String pokuta;
+        
+        //Input
         System.out.println("--------Úloha č.9--------");
-        System.out.println("Zadejte maximální povolenou rychlost v úseku");
-        int maxPovolenaRychlost = sc.nextInt();
-        System.out.println("Zadejte délku úseku v metrech");
-        int delkaUseku = sc.nextInt();
+        System.out.println("Zadejte maximální povolenou rychlost v úseku [km/h]");
+        maxPovolenaRychlost = sc.nextInt();
+        System.out.println("Zadejte délku úseku v celých metrech");
+        delkaUseku = sc.nextInt();
         System.out.println("Zadejte počáteční časový údaj v tomto pořadí: hodina, minuta, sekunda");
-        int hodPocatek = sc.nextInt();
-        int minPocatek = sc.nextInt();
-        int secPocatek = sc.nextInt();
+        hodPocatek = sc.nextInt();
+        minPocatek = sc.nextInt();
+        secPocatek = sc.nextInt();
         System.out.println("Zadejte koncový časový údaj v tomto pořadí: hodina, minuta, sekunda");
-        int hodKonec = sc.nextInt();
-        int minKonec = sc.nextInt();
-        int secKonec = sc.nextInt();
+        hodKonec = sc.nextInt();
+        minKonec = sc.nextInt();
+        secKonec = sc.nextInt();
+        
+        //výpočty
+        rozdilCasu = (hodKonec*3600 + minKonec*60 + secKonec) - (hodPocatek*3600 + minPocatek*60 + secPocatek);
+        
+        prumRychlost = (double)(delkaUseku/rozdilCasu);
+        prumRychlostKMH = (double)(prumRychlost * 3.6);
+        rozdilRychlosti = (int) (Math.floor(prumRychlostKMH) - maxPovolenaRychlost);
+        
+        vysePokuty = rozdilRychlosti*POKUTA_ZA_KMH;
+        
+        System.out.println("Průměrná rychlost vozidla v úseku dlouhém " + delkaUseku + "m překonaným za " + rozdilCasu + "s je " + prumRychlost + "m/s či " + prumRychlostKMH + "km/h");
+        pokuta = rozdilRychlosti > 0 ? "Řidič vozidla dostane pokutu za rychlost ve výši "+vysePokuty+" !" : "Řidič vozidla nedostane pokutu!";
+        
+        System.out.println(pokuta);
         
     }
     
