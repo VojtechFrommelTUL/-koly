21976b23f5503c55b1cf0437e0b9fc4fe0b75c8e
VojtechFrommelTUL <72199527+VojtechFrommelTUL@users.noreply.github.com>
2020-10-18 11:42:09 +0200

    DU2 - print fix

--- Alg1_du2_frommel/src/alg1_du2_frommel/KvadratickaRovnice.java
+++ Alg1_du2_frommel/src/alg1_du2_frommel/KvadratickaRovnice.java
@@ -17,7 +17,7 @@ public class KvadratickaRovnice {
         double b = sc.nextDouble();
         double c = sc.nextDouble();
         
-        System.out.println("KOntrola diskriminantu:");
+        System.out.println("Kontrola diskriminantu:");
         
         //Kontrola D
         double D;
@@ -26,7 +26,7 @@ public class KvadratickaRovnice {
         //0,1,2 řešení - větvení
         if(D<0)
         {
-            System.out.println("Diskriminant je menší, než nula -> Rovnice není řešitelná v oboru R");
+            System.out.println("Diskriminant je menší, než nula (" + D +") -> Rovnice není řešitelná v oboru R");
         }
         else if(D==0)
         {
@@ -41,9 +41,10 @@ public class KvadratickaRovnice {
         {
             reseni[0] = (-b + Math.sqrt(D)) / (2*a);
             reseni[1] = (-b - Math.sqrt(D)) / (2*a);
-            System.out.println("Diskriminant je kladný -> Rovnice má dvě POTENCIONÁLNÍ řešení");
+            System.out.println("Diskriminant je kladný (" + D +") -> Rovnice má dvě POTENCIONÁLNÍ řešení");
             System.out.println("x1 = " + reseni[0]);
             System.out.println("x2 = " + reseni[1]);
+            
             System.out.println("KONTROLA ŘEŠENÍ");
             if(a*Math.pow(reseni[0], 2)+b*reseni[0] + c == 0) System.out.println("Pro x1 PLATÍ rovnost -> je řešením KVR");
             else System.out.println("Pro x1 NEPLATÍ rovnost -> není řešením KVR");
